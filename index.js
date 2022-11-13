176b10f89c42c06d5312d9767bcd246317fccb23
mamun4447 <abdullah15-12557@diu.edu.bd>
2022-11-14 00:44:32 +0600

    connect with mongodb,get&post methods

--- index.js
+++ index.js
@@ -1,26 +1,88 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
-app.use(cors());
-
 const Port = process.env.PORT || 5000;
-const data = require("./data/services.json");
+
+app.use(bodyParser());
+app.use(cors());
+app.use(express());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri =
+  "mongodb+srv://mongodb1:4VySxSVM7QhQu3xp@mycluster0.exc7x.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connected to mongo");
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+run().catch((error) => console.error(error.message));
+
+const Services = client.db("finalProject").collection("services");
+const Provider = client.db("finalProject").collection("provider");
+
+app.get("/services", async (req, res) => {
+  try {
+    const cursor = Services.find({});
+    const result = await cursor.toArray();
+    res.send({
+      success: true,
+      message: "Got all data",
+      datas: result,
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+app.get("/services/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const result = await Services.findOne(query);
+    res.send({
+      success: true,
+      message: "got single data",
+      datas: result,
+    });
+  } catch (error) {
+    res.send({ seccess: false, message: error.message });
+  }
+});
+
+app.post("/provider", async (req, res) => {
+  try {
+    await Provider.insertOne(req.body);
+    res.send({
+      success: true,
+      message: "Successfully created account!",
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: error,
+    });
+    console.log(error);
+  }
+});
 
 app.listen(Port, () => {
   console.log("server is running", Port);
 });
 app.get("/", (req, res) => {
   res.send("Got your data");
-});
-
-app.get("/services", (req, res) => {
-  res.send(data);
-});
-app.get("/services/:id", (req, res) => {
-  const id = req.params.id;
-  const singledata = data.filter((n) => n.id == id);
-  res.send(singledata);
 });
 
 module.exports = app;
